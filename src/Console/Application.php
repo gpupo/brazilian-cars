ca37b827c783315d890bc46d1d404b83aa62b04f
Gilmar Pupo <contact@gpupo.com>
2019-01-31 16:13:38 -0200

    Add latest list

---
+++ src/Console/Application.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of gpupo/brazilian-cars
+ * Created by Gilmar Pupo <contact@gpupo.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <https://opensource.gpupo.com/>.
+ *
+ */
+
+namespace Gpupo\BrazilianCars\Console;
+
+use Gpupo\BrazilianCars\Factory;
+use Gpupo\CommonSdk\Console\AbstractApplication;
+use Gpupo\CommonSdk\FactoryInterface;
+use Psr\Log\LoggerInterface;
+use Psr\SimpleCache\CacheInterface;
+
+final class Application extends AbstractApplication
+{
+    public function factorySdk(array $options, LoggerInterface $logger = null, CacheInterface $cache = null): FactoryInterface
+    {
+        $this->define('ENDPOINT_DOMAIN', $options['ENDPOINT_DOMAIN']);
+
+        return new Factory($options, $logger, $cache);
+    }
+}
