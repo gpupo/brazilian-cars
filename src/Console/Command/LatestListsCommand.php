68cb61d7ab9f395d21e5cf9f80ee7da26ab6e559
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2019-01-31 16:13:38 -0200

    Add latest list

---
+++ src/Console/Command/LatestListsCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of gpupo/brazilian-cars
+ * Created by Gilmar Pupo <contact@gpupo.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <https://opensource.gpupo.com/>.
+ *
+ */
+
+namespace Gpupo\BrazilianCars\Console\Command;
+
+use Gpupo\Common\Traits\TableTrait;
+use Gpupo\CommonSdk\Console\Command\AbstractCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class LatestListsCommand extends AbstractCommand
+{
+    use TableTrait;
+
+    protected function configure()
+    {
+        $this
+            ->setName('lists:latest')
+            ->setDescription('Tabelas de referência mais recentemente disponibilizadas');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = $this->getFactory()->factoryManager('main');
+        $collection = $manager->getLists();
+        $l = array_values($collection->first());
+        $output->writeln(sprintf('Latest Table is <info>%s</> published in <fg=yellow>%s</>', ...$l));
+        $this->displayTableResults($output, $collection->slice(0, 3));
+    }
+}
---
+++ src/Factory.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of gpupo/brazilian-cars
+ * Created by Gilmar Pupo <contact@gpupo.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <https://opensource.gpupo.com/>.
+ *
+ */
+
+namespace Gpupo\BrazilianCars;
+
+use Gpupo\BrazilianCars\Client\Client;
+use Gpupo\CommonSdk\Entity\GenericManager;
+use Gpupo\CommonSdk\FactoryAbstract;
+
+class Factory extends FactoryAbstract
+{
+    protected $name = 'brazilian-cars';
+
+    public function setClient(?array $clientOptions = [])
+    {
+        $this->client = new Client($clientOptions, $this->getLogger(), $this->getSimpleCache());
+    }
+
+    public function getNamespace()
+    {
+        return  '\\'.__NAMESPACE__.'\Entity\\';
+    }
+
+    protected function getSchema(): array
+    {
+        return [
+            'generic' => [
+                'manager' => GenericManager::class,
+            ],
+            'main' => [
+                'manager' => Entity\MainManager::class,
+            ],
+        ];
+    }
+}
