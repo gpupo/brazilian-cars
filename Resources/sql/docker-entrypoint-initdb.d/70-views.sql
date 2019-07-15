9e7bf28587e3045c0d59d7a07e741a5ff6eac872
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2019-07-15 10:39:07 -0300

    Add docker-entrypoint-initdb.d

---
+++ Resources/sql/docker-entrypoint-initdb.d/70-views.sql
@@ -0,0 +1,11 @@
+
+DROP VIEW IF EXISTS `bc_manufacturer`;
+
+CREATE VIEW `bc_manufacturer`
+ AS select distinct `bc_vehicle`.`manufacturer_id` AS `manufacturer_id`,`bc_vehicle`.`manufacturer` AS `manufacturer` from `bc_vehicle` order by `bc_vehicle`.`manufacturer`;
+
+
+ DROP VIEW IF EXISTS `bc_manufacturer_count`;
+
+ CREATE VIEW `bc_manufacturer_count`
+ AS select `a`.`manufacturer` AS `manufacturer`,(select count(0) from `bc_vehicle` `b` where `b`.`manufacturer_id` = `a`.`manufacturer_id`) AS `vehicles` from `bc_manufacturer` `a` order by (select count(0) from `bc_vehicle` `b` where `b`.`manufacturer_id` = `a`.`manufacturer_id`) desc;
